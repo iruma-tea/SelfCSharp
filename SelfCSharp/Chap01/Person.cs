82868a4f11a9040a2c2ad3d8f0a43f516a1ca943
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-11-18 21:40:47 +0900

    ドキュメンテーションコメントを付与

--- SelfCSharp/Chap01/Person.cs
+++ SelfCSharp/Chap01/Person.cs
@@ -1,11 +1,25 @@
 namespace SelfCSharp.Chap01 
 {
+    /// <summary>
+    /// 人物の情報を表す
+    /// </summary>
     internal class Person
     {
+        /// <summary>
+        /// 人物の苗字
+        /// </summary>
         public string? firstName;
 
+        /// <summary>
+        /// 人物の名前
+        /// </summary>
         public string? lastName;
 
+        /// <summary>
+        /// 氏名を表示する
+        /// </summary>
+        /// <param name="Age">年齢</param>
+        /// <returns>氏名と年齢に基づいて整形された文字列</returns>
         public string Show(int Age)
         {
             return $"名前は{lastName}{firstName}、{Age}歳です。";
