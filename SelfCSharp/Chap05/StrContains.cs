63d452388fa38d4bfa1b6cea609b6df068b01a7e
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-11-26 18:40:51 +0900

    リスト5.13まで学習完了

---
+++ SelfCSharp/Chap05/StrContains.cs
@@ -0,0 +1,13 @@
+namespace SelfCSharp.Chap05
+{
+    internal class StrContains
+    {
+        static void Main(string[] args)
+        {
+            var str = "WINGSプロジェクト";
+            Console.WriteLine(str.Contains("プロ"));
+            Console.WriteLine(str.StartsWith("WINGS"));
+            Console.WriteLine(str.EndsWith("WINGS"));
+        }
+    }
+}
