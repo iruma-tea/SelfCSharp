390025940f39a5de94558c8fd6d8963afffd1ce0
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-29 21:51:58 +0900

    Chap01の学習完了

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SelfCSharp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string? name = Console.ReadLine();
+        Console.WriteLine($"こんにちは、{name}さん！");
+    }
+}
