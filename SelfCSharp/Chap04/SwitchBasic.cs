a9aabce11141067e341782b8ae47f9ef9c31fdbe
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-30 22:28:05 +0900

    リスト4-10まで学習完了

---
+++ SelfCSharp/Chap04/SwitchBasic.cs
@@ -0,0 +1,24 @@
+﻿namespace SelfCSharp.Chap04;
+
+public class SwitchBasic
+{
+    static void Main(string[] args)
+    {
+        var rank = "甲";
+        switch (rank)
+        {
+            case "甲":
+                Console.WriteLine("大変良いです。");
+                break;
+            case "乙":
+                Console.WriteLine("良いです。");
+                break;
+            case "丙":
+                Console.WriteLine("がんばりましょう。");
+                break;
+            default:
+                Console.WriteLine("???");
+                break;
+        }
+    }
+}
