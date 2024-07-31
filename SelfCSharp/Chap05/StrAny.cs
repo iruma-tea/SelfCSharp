343fe4274b39ca7e543c266d1769fab1ab707870
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-31 16:58:17 +0900

    リスト5.9まで学習完了

---
+++ SelfCSharp/Chap05/StrAny.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfCSharp.Chap05;
+
+public class StrAny
+{
+    static void Main(string[] args)
+    {
+        var str = "WINGS2号";
+        Console.WriteLine(str.Any(ch => Char.IsDigit(ch)));
+    }
+}
