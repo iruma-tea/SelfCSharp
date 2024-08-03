95803f74999b1cbde7e3d598aef3c8852e58cc85
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-08-03 17:42:29 +0900

    リスト5-17まで学習完了

---
+++ SelfCSharp/Chap05/Practice/PSubstr.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfCSharp.Chap05.Practice;
+
+public class PSubstr
+{
+    static void Main(string[] args)
+    {
+        var str = "プログラミング言語";
+        Console.WriteLine(str.Substring(4, 3));
+    }
+}
