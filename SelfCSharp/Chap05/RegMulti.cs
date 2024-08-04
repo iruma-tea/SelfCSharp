e5523a86100a3bf2a0766439b6338e9c49749282
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-08-04 23:27:58 +0900

    リスト5.24まで学習完了

---
+++ SelfCSharp/Chap05/RegMulti.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SelfCSharp.Chap05;
+
+public class RegMulti
+{
+    static void Main(string[] args)
+    {
+        var str = "10人のインディアン。\n1年生になったら";
+        var rgx = new Regex(@"^\d+", RegexOptions.Multiline);
+        var result = rgx.Matches(str);
+
+        foreach (Match m in result)
+        {
+            Console.WriteLine(m.Value);
+        }
+    }
+}
---
+++ SelfCSharp/Chap05/RegSingle.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SelfCSharp.Chap05;
+
+public class RegSingle
+{
+    static void Main(string[] args)
+    {
+        var str = "初めして。\nよろしくお願いします。";
+        var rgx = new Regex(@"^.+", RegexOptions.Singleline);
+        var result = rgx.Matches(str);
+
+        foreach (Match m in result)
+        {
+            Console.WriteLine(m.Value);
+        }
+    }
+}
