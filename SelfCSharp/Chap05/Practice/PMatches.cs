12bfbe7f6fe96ff63d7ccabdd9844edd119de1c4
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-08-05 16:47:23 +0900

    5.2の学習完了

---
+++ SelfCSharp/Chap05/Practice/PMatches.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SelfCSharp.Chap05.Practice;
+
+public class PMatches
+{
+    static void Main(string[] args)
+    {
+        var str = "住所は〒184-0000 鎌ヶ谷市梶野町0-0-0です。\nあなたの住所は〒273-0000 嬬恋市大野町0-9-9ですね";
+        var rgx = new Regex(@"\d{3}-\d{4}");
+        var result = rgx.Matches(str);
+        foreach (Match m in result)
+        {
+            Console.WriteLine(m.Value);
+        }
+    }
+}
