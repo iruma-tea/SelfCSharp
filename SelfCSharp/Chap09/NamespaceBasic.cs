254df86e1b723aa68168d1592d00e1dc9ef1307b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-08-14 17:37:36 +0900

    Chap09の学習開始

---
+++ SelfCSharp/Chap09/NamespaceBasic.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace SelfCSharp.Chap09.Ns;
+
+public class NamespaceBasic
+{
+
+}
+
+//namespace SelfCSharp
+//{
+//    namespace Chap09
+//    {
+//        namespace Ns
+//        {
+//            internal class NamespaceBasic
+//            {
+//            }
+//        }
+//    }
+//}
