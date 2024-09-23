ac371012c8a551bb34a7840b73462de8cdb704b2
iruma-tea <shusuke.think.pad@gmail.com>
2024-09-23 18:57:41 +0900

    リスト10.22まで学習完了

---
+++ SelfCSharp/Chap10/LinqQuery.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace SelfCSharp.Chap10;
+
+public class LinqQuery
+{
+    static void Main(string[] args)
+    {
+        // クエリ構文
+        var bs = from b in AppTables.Books where b.Price < 3000 select b.Title;
+
+        //var bs = from b in AppTables.Books
+        //         where b.Price < 3000
+        //         select new { Title = b.Title, Price = b.Price };
+
+        //メソッド構文
+        //var bs = AppTables.Books
+        //    .Where(b => b.Price < 3000)
+        //    .Select(b => b.Title);
+
+        foreach (var b in bs)
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
