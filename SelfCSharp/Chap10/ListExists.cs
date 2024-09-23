2ae0b14b6e586b5e86ebae30ab8d0db21a905243
iruma-tea <shusuke.think.pad@gmail.com>
2024-09-23 18:25:00 +0900

    10.1 デリゲート／匿名メソッド／ラムダ式の学習完了

---
+++ SelfCSharp/Chap10/ListExists.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SelfCSharp.Chap10;
+
+public class ListExists
+{
+    static void Main(string[] args)
+    {
+        var list = new List<string>{"からすなぜ鳴くの", "からすは山に",
+                "可愛い七つの", "子があるからよ"};
+        Console.WriteLine(list.Exists(str => str.Length >= 7));
+        Console.WriteLine(list.TrueForAll(str => str.Length < 10));
+    }
+}
---
+++ SelfCSharp/Chap10/ListRemove.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SelfCSharp.Chap10;
+
+public class ListRemove
+{
+    static void Main(string[] args)
+    {
+        var list = new List<int> { 1, -15, 30, 60, -50, 40 };
+        list.RemoveAll(v => v < 0);
+
+        foreach (var n in list)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
