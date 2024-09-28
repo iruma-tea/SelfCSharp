24c49855caf5d0bf1308920096d89b17029e4794
iruma-tea <shusuke.think.pad@gmail.com>
2024-09-28 17:51:03 +0900

    Chap10の学習完了

--- SelfCSharp/Chap10/LinqInto.cs
+++ SelfCSharp/Chap10/LinqInto.cs
@@ -4,5 +4,28 @@ namespace SelfCSharp.Chap10;
 
 public class LinqInto
 {
+    static void Main(string[] args)
+    {
+        // クエリ構文
+        var bs = from b in AppTables.Books
+                 group b by b.Publisher into pubs
+                 where pubs.Average(b => b.Price) >= 3500
+                 select new
+                 {
+                     Published = pubs.Key,
+                     AveragePrice = pubs.Average(b => b.Price)
+                 };
 
+        // メソッド構文
+        // var bs1 = AppTables.Books.GroupBy(b => b.Publisher).Where(pubs => pubs.Average(b => b.Price) >= 3500).Select(pubs => new
+        // {
+        //     Published = pubs.Key,
+        //     AveragePrice = pubs.Average(b => b.Price)
+        // });
+
+        foreach (var b in bs)
+        {
+            Console.WriteLine($"{b.Published} {b.AveragePrice}円");
+        }
+    }
 }
